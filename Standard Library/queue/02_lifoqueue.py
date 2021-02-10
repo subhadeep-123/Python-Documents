912ecc9b5543b37d032b92a33eada09bd0b9c7f3
Subhadeep Banerjee <subhadeep@klizos.com>
2021-02-11 01:37:25 +0530

    Gonna Organize Code No-worries

--- Standard Library/queue/02_lifoqueue.py
+++
@@ -1,8 +0,0 @@
-import queue
-
-q = queue.LifoQueue()
-
-for i in range(10):
-    q.put(i)
-for i in range(10):
-    print(q.get())
--- Standard Library/queue/02_priority_queue.py
+++
@@ -1,9 +0,0 @@
-import queue
-q = queue.PriorityQueue()
-q.put((2, "Hello World"))
-q.put((11, 11))
-q.put((5, 8.55))
-q.put((1, True))
-
-while not q.empty():
-    print(q.get()[1])
